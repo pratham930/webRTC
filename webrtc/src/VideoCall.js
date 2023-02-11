cecd5cb3c5d0d89463bb2de3900d0fd3e4dc61ec
pratham930 <priyatam.patel17@gmail.com>
2023-02-11 01:10:42 -0800

    again did everything maualy
    again did everything maualy

---
+++ webrtc/src/VideoCall.js
@@ -0,0 +1,159 @@
+import { Button, Box } from "@mui/material";
+import React, { useRef, useState, useEffect } from "react";
+
+const VideoCall = () => {
+
+    const localVideoRef = useRef();
+    const remoteVideoRef = useRef();
+    const textRef = useRef(); 
+    const pc = useRef(new RTCPeerConnection(null));
+
+
+
+    useEffect(() => {
+        const constrains ={
+            audio:true,
+            video:true
+          }
+        
+             navigator.mediaDevices.getUserMedia(constrains)
+          .then(stream=>{
+            localVideoRef.current.srcObject = stream
+
+            stream.getTracks().forEach(track=>{
+                _pc.addTrack(track,stream)
+            })
+            // localAudioRef.current.srcObject = stream
+          }).catch(e=>{ console.log("first",e)})
+
+
+
+          const _pc = new RTCPeerConnection(null);
+          
+              _pc.onicecandidate =  (e) => {
+          if(e.candidate)
+            console.log(JSON.stringify(e.candidate),"121")   
+              };
+          
+              _pc.oniceconnectionstatechange = (e) => {
+                 console.log(e,"oniceconnection");
+              };
+          
+              _pc.ontrack = (e) => {
+                console.log(e,"e")
+
+remoteVideoRef.current.srcObject = e.streams[0]
+
+
+                //   remoteVideoRef.current.srcObject = e.streams[0];
+                /// we got remote stream
+              };
+
+              pc.current = _pc;
+
+    }, [])
+
+
+    const createOffer =()=>{
+
+        pc.current
+        .createOffer({
+          offerToReceiveAudio: 1,
+          offerToReceiveAudio: 1,
+        })
+        .then((sdp) => {
+          console.log(JSON.stringify(sdp));
+          pc.current.setLocalDescription(sdp);
+  
+         
+        })
+        .catch((e) => console.log(e));
+
+
+
+    }
+     const createAnswer =()=>{
+        pc.current
+        .createAnswer({
+          offerToReceiveAudio: 1,
+          offerToReceiveAudio: 1,
+        })
+        .then((sdp) => {
+          console.log(JSON.stringify(sdp));
+          pc.current.setLocalDescription(sdp);
+        })
+        .catch((e) => console.log(e));
+    }
+    const setRemoteArea =()=>{
+        const sdp = JSON.parse(textRef.current.value);
+        pc.current.setRemoteDescription(new RTCSessionDescription(sdp));
+        console.log(sdp)
+    }
+    const addCandidate =()=>{
+        const candidate = JSON.parse(textRef.current.value);
+        console.log('adding candidate:',candidate)
+    pc.current.addIceCandidate(new RTCIceCandidate(candidate));
+
+    }
+
+
+
+
+  return (
+    <div>
+{/* ///***TO START VIDEO Manually */}
+      {/* <Button onClick={()=>getUserMedia()} variant="contained" >
+click
+    </Button> */}
+
+      {/* ////8888***?? */}
+<Box sx={{display:"flex" ,m:'10px'}}>
+<video
+          style={{ width: 200, height: 240, backgroundColor: "black" }}
+          ref={localVideoRef}
+          autoPlay
+        >
+         
+        </video>
+
+        <video
+          style={{ width: 200, height: 240, backgroundColor: "black" ,marginLeft:"10px" }}
+          ref={remoteVideoRef}
+          autoPlay
+        >
+        
+        </video>
+</Box>
+
+<Box>
+<Box>
+          <textarea ref={textRef}></textarea>
+        </Box>
+</Box>
+
+
+<Box>
+        <Button onClick={createOffer} variant="contained">
+          offer
+        </Button>
+
+       
+        <Button onClick={createAnswer} variant="contained">
+          answer
+        </Button>
+
+        <Button onClick={setRemoteArea} variant="contained">
+          setting remote
+        </Button>
+
+        <Button onClick={addCandidate} variant="contained">
+          adding candidate
+        </Button>
+      </Box>
+
+
+    </div>
+  )
+}
+
+export default VideoCall
--- webrtc/src/index.js
+++ webrtc/src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Socket from './Socket';
+// import Socket from './Socket';
+import VideoCall from './VideoCall';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <Socket/> */}
-    <App />
+<VideoCall/>
+    {/* <App /> */}
   </React.StrictMode>
 );
 
